6c4f523f1e2aab7e7d1c72272f74a96b1a05ac7b
danielogen <danielogen@gmail.com>
2023-08-03 12:40:30 +0300

    ft: add constants package

---
+++ src/constants/constant.py
@@ -0,0 +1,13 @@
+class Constant:
+
+    # GitHub constants
+    BASE_URL = "https://api.github.com/repos/"
+
+    # common.py
+    # global variables
+    NGRAM_SIZE   = 4
+    CONTEXT_LINE = 10
+    VERBOSE_MODE = False
+    MAGIC_COOKIE = None
+    BLOOMFILTER_SIZE = 2097152
+    MIN_MN_RATIO = 32
