544d88a4cfb65ac8f6fb4c7d13826b508c66a5a5
danielogen <danielogen@gmail.com>
2023-10-04 14:48:56 -0700

    ft: using issues endpoint instead of pulls

--- src/core/patchExtractor.py
+++ src/core/patchExtractor.py
@@ -9,7 +9,7 @@ class GetOutOfLoop(Exception):
     pass
 
 
-def pr_patches(repo, diverge_date, least_date, token_list, ct):
+def pullrequest_patches(repo, diverge_date, least_date, token_list, ct):
     '''
     Get details of pull requests which part of patches 
 
@@ -27,62 +27,55 @@ def pr_patches(repo, diverge_date, least_date, token_list, ct):
     pr = []
     pr_all_merged = []
     title = []
-    tot_com = 0
+
     bug_keyword = helpers.unique(constant.BUGFIX_KEYWORDS)
 
-    p = 1
-    count = 0
+    page_number = 1
     try:
         while True:
-            url = f"{constant.GITHUB_BASE_URL}{repo}/pulls?page={str(p)}&per_page=100&state=closed&sort=created&direction=desc"
-            p = p + 1
-            count = 1
+            url = f"{constant.GITHUB_BASE_URL}{repo}/issues?page={str(page_number)}&per_page=100&state=closed&since={diverge_date}"
+            page_number = page_number + 1
 
-            pulls_arrays, ct = helpers.get_response(url, token_list, ct)
-            # tot_com = tot_com + len(pulls_arrays)
-            # print(url)
-            if pulls_arrays is not None:
+            issues_data, ct = helpers.get_response(url, token_list, ct)
+
+            if issues_data is not None:
                 print(url)
-                # pr = 0
-                if len(pulls_arrays) == 0:
+                # end loop is you start getting empty data array
+                if len(issues_data) == 0:
                     break
-                for pull_obj in pulls_arrays:
-                    # I added a try and accept block since this step would yield and exception of the repo 
-                    # pair apache/kafka->linkedin/kafka. For some reason, the exception was not being raised with other 
-                    # repo pairs like learningequality/pycaption->pbs/pycaption and 
-                    # hzdg/django-enumfields->druids/django-choice-enumfields
-                    # I guess there are some commits in apache/kafka->linkedin/kafka that were causing the error. 
-                    # A workaround was to ignore them the pull requests that were yielding the exception
+                for issue in issues_data:
                     try:
-                        pull_created_at = pull_obj['created_at']
+                        # filter pull request from issues 
+                        if 'pull_request' not in issue:
+                            continue
+
+                        # skip closed but not merged pull requests 
+                        if issue['pull_request']['merged_at'] is None:
+                            continue
+                            
+                        pull_created_at = issue['created_at']
+                        pull_merged_at = issue['pull_request']['merged_at']
+
                         if parser.parse(pull_created_at) > parser.parse(least_date):
                             continue
-                            # raise GetOutOfLoop
-
-                        if pull_obj['merged_at'] is not None:
-                            if parser.parse(pull_obj['merged_at']) > parser.parse(least_date):
-                                continue
-                            # print(repo, 'parser.parse(pull_obj[merged_at]) = ', parser.parse(pull_obj['merged_at']))
-                            # print(repo, 'parser.parse(diverge_date) = ', parser.parse(diverge_date))
-
-                            if parser.parse(pull_obj['merged_at']) < parser.parse(diverge_date):
-                                count = 0
+                        if parser.parse(pull_merged_at) > parser.parse(least_date):
+                            continue
+                            
+                        if parser.parse(pull_merged_at) < parser.parse(diverge_date):
+                            
+                            return pr, title, pr_all_merged, ct
+                                                    
+                        pr_all_merged.append(issue['number'])
+                        pull_title = issue['title'].lower().replace(';', '-').replace(',', '-')
+                        for bug in bug_keyword:
+                            if bug in pull_title:
+                                pr.append(issue['number'])
+                                title.append(pull_title)
                                 break
-                                # raise GetOutOfLoop
-                            pr_all_merged.append(pull_obj['number'])
-                            pull_title = pull_obj['title'].lower().replace(';', '-').replace(',', '-')
-                            for bug in bug_keyword:
-                                if bug in pull_title:
-                                    pr.append(pull_obj['number'])
-                                    title.append(pull_title)
-                                    break
-                        if count == 0:
-                            break
                     except Exception as e:
-                        print(e)
-            if count == 0:
-                break
+                        print("Error... ", e)
+            
     except GetOutOfLoop:
-        pass
-
+        print("Stopping data extraction....")
+    
     return pr, title, pr_all_merged, ct
