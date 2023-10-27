22c8e47b71a5be064f8b5ab45dceaff95efc5c95
danielogen <danielogen@gmail.com>
2023-10-26 20:30:51 -0700

    ch(refactor): refactor helper functions

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -38,6 +38,7 @@ def get_response(url, token_list, ct):
         ct += 1
     except Exception as e:
         print(e)
+        print("Error in func: [get_response]...")
     return json_data, ct
 
 
@@ -53,8 +54,11 @@ def api_request(url, token):
     '''
     header = {'Authorization': 'token %s' % token}
     response = requests.get(url, headers=header)
-    json_response = json.loads(response.content)
-    return json_response
+    try:
+        json_response = json.loads(response.content)
+        return json_response
+    except Exception as e:
+        return response
 
 
 def repo_commit_date(repo, date, token_list, ct):
@@ -67,6 +71,7 @@ def repo_commit_date(repo, date, token_list, ct):
         sha = content_arrays[0]['sha']
     except Exception as e:
         print(e)
+        print("Error in func: [repo_commit_date]...")
     # commit_date = content_arrays[0]['commit']['committer']['date']
     return sha, ct
 
@@ -126,6 +131,7 @@ def divergence_date(mainline, variant, token_list, ct, least_date='', diverge_da
 
     except:
         print(f'{constant.GITHUB_API_BASE_URL}{mainline}/compare/{sha_ml}...{fork1[0]}:{sha_vr}')
+        print("Error in func: [divergence_date]...")
 
     return fork_date, diverge_date, least_date, ahead, behind, ct
 
@@ -352,7 +358,7 @@ def get_first_last_commit(repo, pullrequest, token_list, ct):
                 commit_sha = commit['sha']
                 commits.append(commit_sha)
     except GetOutOfLoop:
-        print("Stopping data extraction....")
+        print("Stopping data extraction in func: [get_first_last_commit]....")
 
     first_commit = commits[0]
     last_commit = commits[-1]
